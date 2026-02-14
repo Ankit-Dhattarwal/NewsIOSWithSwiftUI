f1d9effa08dcd84044f48014d1df33da4911c353
AnkitDhattarwal <ankitdhatterwal09@gmail.com>
2026-02-14 14:10:08 +0530

    news app with swift ui

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Ankit on 14/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List (networkManager.posts) { post in
+                Text(post.title)
+            }
+            .onAppear(perform: {
+                self.networkManager.fetchData()
+            })
+            
+        }
+    }
+}
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjor"),
+//    Post(id: "3", title: "Halo"),
+//]
+
+#Preview {
+    ContentView()
+}
---
+++ H4XOR News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Ankit on 14/02/26.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
